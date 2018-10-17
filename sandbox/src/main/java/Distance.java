3749448e841f4efe34c86cea1c69748d306ca468
erumiant <katerinka_ru93@bk.ru>
2018-10-17 19:38:26 +0300

    class Distance was created to show the result

---
+++ sandbox/src/main/java/Distance.java
@@ -0,0 +1,10 @@
+
+public class Distance {
+
+    public static void main(String[] args) {
+        Point x = new Point(4.0, 3.0);
+        System.out.println("Засстояние между двумя точками " + x.p1 + " и " + x.p2 +" равно " + x.distance());
+    }
+
+
+}
