09b575962436aa8c8c9ae980f14eac047cb05cb2
erumiant <katerinka_ru93@bk.ru>
2018-10-17 19:49:43 +0300

    changes were done for first task

---
+++ sandbox/src/main/java/pack/task1/firstProgram.java
@@ -0,0 +1,12 @@
+package pack.task1;
+
+class firstProgram {
+    public static void main(String[] args) {
+        hello("World!");
+        hello("Katya");
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody + "!");
+    }
+}
