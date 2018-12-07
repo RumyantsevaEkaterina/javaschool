48b56c8489160598c7da62cd539f908b1b82b1a5
erumiant <katerinka_ru93@bk.ru>
2018-12-07 19:56:47 +0300

    Update Navigation

--- adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/HelperBase.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/HelperBase.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.adressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -18,11 +19,23 @@ public class HelperBase {
 
     protected void type(By locator, String text) {
         click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
+        if (text!=null) {
+            String existingText=wd.findElement(locator).getAttribute("value");
+            if (! text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
     }
 
-
+    protected boolean isElementPresent(By locator) {
+        try {
+            wd.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
 
     public boolean isAlertPresent() {
         try {
--- adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTest.java
@@ -10,7 +10,7 @@ public class GroupCreationTest extends TestBase {
     public void testCreateGroup() throws Exception {
         app.getNavigationHelper().goToGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupCreation(new GroupData("test", "testhead", "123"));
+        app.getGroupHelper().fillGroupCreation(new GroupData("test", "test3", null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
 
