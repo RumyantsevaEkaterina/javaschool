90c138e83a43577c174a7636847e28e53be8c7d3
erumiant <katerinka_ru93@bk.ru>
2018-12-07 20:22:27 +0300

    precondition checks was created for Group modification and creation

--- adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/DeleteGroupTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/DeleteGroupTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.adressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.adressbook.model.GroupData;
 
 public class DeleteGroupTest extends TestBase {
 
@@ -8,6 +9,9 @@ public class DeleteGroupTest extends TestBase {
   @Test
   public void testDeleteGroup() {
     app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteGroups();
     app.getGroupHelper().returnToGroupPage();
--- adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTest.java
@@ -9,10 +9,7 @@ public class GroupCreationTest extends TestBase {
     @Test
     public void testCreateGroup() throws Exception {
         app.getNavigationHelper().goToGroupPage();
-        app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupCreation(new GroupData("test", "test3", null));
-        app.getGroupHelper().submitGroupCreation();
-        app.getGroupHelper().returnToGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("test", "test3", null));
 
     }
 
