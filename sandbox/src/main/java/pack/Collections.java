3296bfc6197cb25c76b490a632375c830960b48c
erumiant <katerinka_ru93@bk.ru>
2018-12-12 17:53:45 +0300

    collections class

---
+++ sandbox/src/main/java/pack/Collections.java
@@ -0,0 +1,17 @@
+package pack;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+    public static void main(String[] args) {
+        String[] langs = {"Java","C#","Python","PHP"};
+
+        List<String> languages = Arrays.asList("Java","C#","Python","PHP");
+
+        for (String l: languages) {
+            System.out.println("Z[jxe dsexbnm " + l);
+        }
+    }
+
+}
