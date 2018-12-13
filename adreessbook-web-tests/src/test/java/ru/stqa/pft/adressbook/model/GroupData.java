2a8736af1ca4b8d6514624ff74e1bd5b3c37bd9c
erumiant <katerinka_ru93@bk.ru>
2018-12-13 16:17:32 +0300

    Group test with collections are modified

--- adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
@@ -3,14 +3,28 @@ package ru.stqa.pft.adressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    public GroupData(int id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -25,10 +39,15 @@ public class GroupData {
         return footer;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "GroupData{" +
-                "name='" + name + '\'' +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
                 '}';
     }
 
@@ -36,12 +55,15 @@ public class GroupData {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         GroupData groupData = (GroupData) o;
-        return name!= null ? name.equals(groupData.name): groupData.name==null;
+
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return name != null ? name.hashCode() : 0;
     }
+
 }
