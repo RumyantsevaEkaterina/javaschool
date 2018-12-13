8ad54aee9d7629ecd31f55f06f2acd5382d411a8
erumiant <katerinka_ru93@bk.ru>
2018-12-13 16:06:29 +0300

    creation test update, lambda expressions

--- adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTest.java
@@ -1,9 +1,10 @@
 package ru.stqa.pft.adressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase {
@@ -13,9 +14,16 @@ public class GroupCreationTest extends TestBase {
     public void testCreateGroup() throws Exception {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("test", "test3", null));
+        GroupData group = new GroupData("test222", "testhead1", "543");
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 
