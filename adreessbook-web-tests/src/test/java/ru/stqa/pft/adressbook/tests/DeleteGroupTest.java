ccb20d1f99dd72128030b153ad2d936ee21dd26f
erumiant <katerinka_ru93@bk.ru>
2018-12-14 19:23:37 +0300

    ensurePreconditions for Group tests

--- adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/DeleteGroupTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/DeleteGroupTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.adressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
@@ -8,25 +9,28 @@ import java.util.List;
 
 public class DeleteGroupTest extends TestBase {
 
-
-  @Test
-  public void testDeleteGroup() {
-    app.getNavigationHelper().goToGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.getNavigationHelper().goToGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
     }
-    List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size() - 1);
-    app.getGroupHelper().deleteGroups();
-    app.getGroupHelper().returnToGroupPage();
-    List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() - 1);
 
-    before.remove(before.size() - 1);
-    Assert.assertEquals(after, before);
+    @Test
+    public void testDeleteGroup() {
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
+        app.getGroupHelper().deleteGroups();
+        app.getGroupHelper().returnToGroupPage();
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(after, before);
 
 
-  }
+    }
 
 
 }
