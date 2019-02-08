8ed441e0b72d1bdc089a6da3b0a5135bfcadf082
erumiant <katerinka_ru93@bk.ru>
2019-02-08 18:15:05 +0300

    тест исправлен

--- sandbox/src/test/java/pack/PointTest.java
+++ sandbox/src/test/java/pack/PointTest.java
@@ -16,6 +16,6 @@ public class PointTest {
     public void verifyDistance() {
         Point p1 = new Point(4.0,0.0);
         Point p2 = new Point(0.0,3.0);
-        Assert.assertTrue(p1.distance(p2) == 0.0);
+        Assert.assertTrue(p1.distance(p2) == 5.0);
     } //проверка,что расстояние считается корректно
 }
